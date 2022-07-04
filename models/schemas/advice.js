e888ee49f4c2ead9e994a88858cee29f840da7fa
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-07-04 01:12:25 -0400

    prehistoric commit

---
+++ models/schemas/advice.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+var adviceSchema = new Schema({
+	title : {type: String, required: true}, 
+	author : {type : String, required: true}, 
+	 
+
+
+
+	}
+
+);
