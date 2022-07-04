af6f3b44382e23fa003cd6ef387291e799a8dedb
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-07-04 01:17:26 -0400

    prehistoric commit

--- models/schemas/advice.js
+++ models/schemas/advice.js
@@ -5,10 +5,11 @@ const Schema = mongoose.Schema;
 var adviceSchema = new Schema({
 	title : {type: String, required: true}, 
 	author : {type : String, required: true}, 
-	 
-
-
-
+	URL : {type: String, required: true}, 
+	summary: String, 
+	authorWikiURL : String,
+	date : Date, //when video was uploaded -- maybe stick with just the year or simplified date
+	
 	}
 
 );
